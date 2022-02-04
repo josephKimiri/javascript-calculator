64ca2dbc1642f7cbd971edbdbd6f8fc6e40b5f89
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-02-04 12:17:25 +0300

    Improve the calculator's functionality.

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
+const ESSENTIAL_KEYS_FOR_CALCULATION = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "/", "-", "*"];
 
 function clearScreen() {
     document.getElementById("result").value = "";
 }
 
+function removeLastCharacter(){
+    const text = document.getElementById("result").value;
+
+const editedText = text.slice(0, -1);
+
+document.getElementById("result").value = editedText;
+}
 
 function display(value) {
-    document.getElementById("result").value += value;
+    if (ESSENTIAL_KEYS_FOR_CALCULATION.includes(value)) {
+        document.getElementById("result").value += value;
+    }document.getElementById("result").value
 }
 
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
-    document.getElementById("result").value = q;
+
+   
+
+    if (q !== undefined) {
+        document.getElementById("result").value  = `${q}`;
+    }
 }
+
+document.addEventListener('keydown', function (event) {
+   
+    if (event.key === "Enter") {
+        calculate();
+    } 
+    else if(event.key === "Backspace"){
+        removeLastCharacter();
+    }
+    else {
+        display(event.key);
+    }
+})
